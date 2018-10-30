ceb2d12344a95c4436ee8d7fb9bca57d8ca6bb0d
Mary Ricalde <siskaes515@gmail.com>
2018-10-30 14:48:56 -0400

    Added integration of search criteria ands view in Controller class, also validation and utilities classes

---
+++ src/main/java/com/foundation/search/controller/Validation.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2018 Jalasoft.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jalasoft, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
+
+package com.foundation.search.controller;
+
+/**
+ * This class contains validation methods for entry values
+ *
+ * @author Mary Ricalde
+ * @version 1.0.
+ */
+public class Validation {
+
+    /**
+     * This method verifies that the value of a field if not empty or null.
+     *
+     * @param field String field value to be verified.
+     * @return True if the field is empty or null, false otherwise.
+     */
+    public static boolean isFieldNullOrEmpty(String field){
+        boolean fieldVerification = false;
+        if (field == null || field.isEmpty()) {
+            fieldVerification = true;
+        }
+        return fieldVerification;
+    }
+
+    /**
+     * This method verifies valid path expression. The path requires prefixing
+     * a drive letter, colon and backslash.
+     *
+     * @param pathName Name of the path which is going to be verified. i.e. "C:\Users"
+     * @return True if the path is valid, false otherwise.
+     */
+    public static boolean isValidPath(String pathName){
+        boolean pathVerification = false;
+        String pathPattern = "^[a-zA-Z]:\\\\[\\S|*\\S]?.*$";
+        if (isFieldNullOrEmpty(pathName)) {
+            return pathVerification;
+        }
+
+        if (pathName.matches(pathPattern)) {
+            pathVerification = true;
+        }
+        return pathVerification;
+    }
+
+    /**
+     * This method verifies valid file name. A file name could contain alphanumeric,
+     * underscore and dash characters
+     *
+     * @param fileName Name of the file which is going to be verified. i.e. "Document 2"
+     * @return boolean
+     */
+    public static boolean isValidFileName(String fileName){
+        boolean fileVerification = false;
+        String filePattern = "[\\w ?-]*$";
+        if (fileName.matches(filePattern)) {
+            fileVerification = true;
+        }
+        return fileVerification;
+    }
+
+    /**
+     * This method validates the extension of a file, the extension should have a
+     * size between 2 and 4 alphabetical characters only.
+     *
+     * @param extension File extension. i.e. "txt"
+     * @return boolean
+     */
+    public static boolean isValidFileExtension(String extension){
+        boolean extensionVerification = false;
+        String extensionPattern = "^([a-z]){2,4}$";
+        if (extension.matches(extensionPattern)) {
+            extensionVerification = true;
+        }
+        return extensionVerification;
+    }
+
+    /**
+     * This method validates an input size value for a file. The size should be
+     * between a range of 0 to 9 whole numbers.
+     *
+     * @param size File size. i.e. "20"
+     * @return boolean
+     */
+    public static boolean isValidSize(String size){
+        boolean sizeVerification = false;
+        String sizePattern = "^\\d{1,9}+$";
+        if (size.matches(sizePattern)) {
+            sizeVerification = true;
+        }
+        return sizeVerification;
+    }
+}
---
+++ src/main/test/com/foundation/search/controller/ValidationTest.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright (c) 2018 Jalasoft.
+ * 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jalasoft, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jalasoft.
+ */
+
+package com.foundation.search.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * This class contains unit tests for field validations
+ *
+ * @author Mary Ricalde
+ * @version 1.0.
+ */
+public class ValidationTest {
+
+    @Test
+    public void validationIsValidPathWithDriveLetterColonAndBackslash() {
+        String path = "C:\\";
+        boolean actualResult = Validation.isValidPath(path);
+        assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void validationIsValidPathWhenValidPathIsIntroduced() {
+        String path = "C:\\files";
+        boolean actualResult = Validation.isValidPath(path);
+        assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void validationIsValidPathWithInvalidDrive(){
+        String path = "@:";
+        boolean actualResult = Validation.isValidPath(path);
+        assertEquals(false, actualResult);
+    }
+
+    @Test
+    public void validationIsValidPathWithMissingColon(){
+        String path = "C\\";
+        boolean actualResult = Validation.isValidPath(path);
+        assertEquals(false, actualResult);
+    }
+
+    @Test
+    public void validationIsValidPathWithMissingBackslash() {
+        String path = "C:";
+        boolean actualResult = Validation.isValidPath(path);
+        assertEquals(false, actualResult);
+    }
+
+    @Test
+    public void validationIsFieldNullOrEmptyWhenPathIsNull(){
+        String path = null;
+        boolean actualResult = Validation.isFieldNullOrEmpty(path);
+        assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void validationIsFieldNullOrEmptyWhenPathIsEmpty(){
+        String path = "";
+        boolean actualResult = Validation.isFieldNullOrEmpty(path);
+        assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void validationIsValidFileNameWhenItIsAlphanumeric() {
+        String fileName = "text 12";
+        boolean actualResult = Validation.isValidFileName(fileName);
+        assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void validationIsValidFileNameWithDash() {
+        String fileName = "text-t12";
+        boolean actualResult = Validation.isValidFileName(fileName);
+        assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void validationIsValidFileNameWithUnderscore() {
+        String fileName = "text_t12";
+        boolean actualResult = Validation.isValidFileName(fileName);
+        assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void validationIsValidFileNameWithInvalidCharacters() {
+        String fileName = "text1@+";
+        boolean actualResult = Validation.isValidFileName(fileName);
+        assertEquals(false, actualResult);
+    }
+
+    @Test
+    public void validationIsValidFileExtensionWithLengthOf2() {
+        // expected result should be true with extension of 2 or 4 characters
+        String extension = "sh";
+        boolean actualResult = Validation.isValidFileExtension(extension);
+        assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void validationIsValidFileExtensionWithLengthOf4() {
+        // expected result should be true with extension of 2 or 4 characters
+        String extension = "xlsx";
+        boolean actualResult = Validation.isValidFileExtension(extension);
+        assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void validationIsValidFileExtensionWithLengthOf6() {
+        String extension = "pdfxls";
+        boolean actualResult = Validation.isValidFileExtension(extension);
+        assertEquals(false, actualResult);
+    }
+
+    @Test
+    public void validationIsValidFileExtensionWithLengthOf1() {
+        String extension = "t";
+        boolean actualResult = Validation.isValidFileExtension(extension);
+        assertEquals(false, actualResult);
+    }
+
+    @Test
+    public void validationIsValidFileExtensionWithNoAlphabeticalText() {
+        String extension = "txt2";
+        boolean actualResult = Validation.isValidFileExtension(extension);
+        assertEquals(false, actualResult);
+    }
+
+    @Test
+    public void validationIsValidFileExtensionWithEmptyText() {
+        String extension = "";
+        boolean actualResult = Validation.isValidFileExtension(extension);
+        assertEquals(false, actualResult);
+    }
+
+    @Test
+    public void validationIsValidSizeWithMaxWholeNumbers() {
+        String size = "999999999";
+        boolean actualResult = Validation.isValidSize(size);
+        assertEquals(true, actualResult);
+    }
+
+    @Test
+     public void validationIsValidSizeWithMinimumSizeNumber() {
+        String size = "0";
+        boolean actualResult = Validation.isValidSize(size);
+        assertEquals(true, actualResult);
+    }
+
+    @Test
+    public void validationIsValidSizeWithoutDefinedLength() {
+        String size = "";
+        boolean actualResult = Validation.isValidSize(size);
+        assertEquals(false, actualResult);
+    }
+
+    @Test
+    public void validationIsValidSizeWithInvalidAlphanumericText() {
+        String size = "22d";
+        boolean actualResult = Validation.isValidSize(size);
+        assertEquals(false, actualResult);
+    }
+
+    @Test
+     public void validationIsValidSizeWhenSizeExceedsOf9Numbers() {
+        String size = "9999999887";
+        boolean actualResult = Validation.isValidSize(size);
+        assertEquals(false, actualResult);
+    }
+
+    @Test
+    public void validationIsValidSizeWhenSpacesAreFound() {
+        String size = "150 5";
+        boolean actualResult = Validation.isValidSize(size);
+        assertEquals(false, actualResult);
+    }
+
+    @Test
+    public void validationIsValidSizeWhenSizeHasDecimals() {
+        String size = "150.25";
+        boolean actualResult = Validation.isValidSize(size);
+        assertEquals(false, actualResult);
+    }
+}
